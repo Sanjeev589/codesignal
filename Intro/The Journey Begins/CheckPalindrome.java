93a20d13ea908a8659c6152cc398e756daeb1624
skuma753 <skuma753@LIN24003549.corp.capgemini.com>
2020-09-22 14:59:03 +0530

    journeyBegins

---
+++ Intro/The Journey Begins/CheckPalindrome.java
@@ -0,0 +1,51 @@
+/*
+
+Given the string, check if it is a palindrome.
+
+Example
+
+For inputString = "aabaa", the output should be
+checkPalindrome(inputString) = true;
+For inputString = "abac", the output should be
+checkPalindrome(inputString) = false;
+For inputString = "a", the output should be
+checkPalindrome(inputString) = true.
+Input/Output
+
+[execution time limit] 3 seconds (java)
+
+[input] string inputString
+
+A non-empty string consisting of lowercase characters.
+
+Guaranteed constraints:
+1 ≤ inputString.length ≤ 105.
+
+[output] boolean
+
+true if inputString is a palindrome, false otherwise.
+
+[Java] Syntax Tips
+
+// Prints help message to the console
+// Returns a string
+// 
+// Globals declared here will cause a compilation error,
+// declare variables inside the function instead!
+String helloWorld(String name) {
+    System.out.println("This prints to the console when you Run Tests");
+    return "Hello, " + name;
+}
+
+*/
+
+
+boolean checkPalindrome(String inputString) {
+StringBuilder sb = new StringBuilder(inputString);
+sb.reverse();
+if(inputString.equals(sb.toString()))
+return true;
+else
+return false;
+
+}
